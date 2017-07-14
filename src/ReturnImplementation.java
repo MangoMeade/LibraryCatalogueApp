6dac911674e65f71dfcef2ebbf1e3a4938cb75e2
jennystpeters <jennystpeters@gmail.com>
2017-07-14 00:22:11 -0400

    Fixed bugs in return book function

--- src/ReturnImplementation.java
+++ src/ReturnImplementation.java
@@ -1,5 +1,5 @@
-import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by jenny on 7/13/2017.
@@ -12,22 +12,34 @@ public class ReturnImplementation {
 
         int returnSelection;
 
-        LibraryImplementation libraryImpl = new LibraryImplementation();
+        HashMap<Integer, Book> bookIndex = new HashMap<>();
+
         ArrayList<Book> checkedoutCatalogue = new ArrayList<>();
 
-        for (Book book: catalogue) {
-            if (book.getStatus()==Status.CHECKED_OUT){
+        for (Book book : catalogue) {
+            if (book.getStatus() == Status.CHECKED_OUT) {
                 checkedoutCatalogue.add(book);
             }
         }
 
-        libraryImpl.printCatalogue(checkedoutCatalogue);
+        if (checkedoutCatalogue.size() > 0) {
 
-        returnSelection = Validator.getInt("Which book would you like to return? (Please enter the line number): ", "Please enter a valid line number: ", 1, checkedoutCatalogue.size());
+            //libraryImpl.printCatalogue(checkedoutCatalogue);
+            bookIndex = printReturnCatalogue(catalogue, checkedoutCatalogue, bookIndex);
 
-        returnToShelf(catalogue.get(returnSelection - 1));
+            returnSelection = Validator.getInt("Which book would you like to return? (Please enter the line number): ", "Please enter a valid line number: ", 1, checkedoutCatalogue.size());
 
-        System.out.println("\nThank you. Your book was successfully returned! Returning to Main Menu.\n");
+            //returnToShelf(catalogue.get(returnSelection - 1));
+            returnToShelf(bookIndex.get(returnSelection));
+
+            System.out.println("\nThank you. Your book was successfully returned! Returning to Main Menu.\n");
+        } else {
+
+            System.out.println("\nThere are currently no books checked out. Returning to Main Menu.\n");
+
+            LibraryImplementation implementation = new LibraryImplementation();
+            implementation.runMainLoop(catalogue);
+        }
     }
 
     public void returnToShelf(Book returnedBook) {
@@ -35,6 +47,26 @@ public class ReturnImplementation {
         returnedBook.setStatus(Status.ON_SHELF);
         System.out.println("\n" + returnedBook.toConsoleFormat());
 
+    }
+
+    public HashMap printReturnCatalogue(ArrayList<Book> catalogue, ArrayList<Book> checkedoutCatalogue, HashMap<Integer, Book> bookIndex) {
+        int i = 1;
+
+        System.out.println("\nCurrent catalogue...");
+        System.out.println();
+
+        for (Book book : checkedoutCatalogue) {
+            System.out.print(i + " " + book.getTitle() + " " + book.getAuthor() + " " + book.getGenre() + " " + book.getBraille() + " " + book.getStatus());
+            bookIndex.put(i, book);
+            i = i + 1;
+            if (book.getStatus() == Status.CHECKED_OUT) {
+                System.out.print(" " + book.getDueDate());
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        return bookIndex;
     }
 
 }
